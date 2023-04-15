d6007af81c80fca128ade84318c86378aa3c46b7
sobirjonovme <huntsman6004@gmail.com>
2023-04-15 08:19:51 +0500

    fix: page size changed in CustomPagination

--- apps/common/pagination.py
+++ apps/common/pagination.py
@@ -3,9 +3,9 @@ from rest_framework.response import Response
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 3
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 5
+    # max_page_size = 20
 
     def get_paginated_response(self, data):
         return Response({
